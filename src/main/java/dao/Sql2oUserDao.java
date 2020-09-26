d54eb3b4aba31df4c115c854b2573ce0b50054e3
Michael-Mbaya <gikundamike@gmail.com>
2020-09-26 15:07:39 +0300

    Data Access Object User

---
+++ src/main/java/dao/Sql2oUserDao.java
@@ -0,0 +1,89 @@
+package dao;
+import models.User;
+import java.util.List;
+import org.sql2o.Sql2o;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+public class Sql2oUserDao implements UserDao{
+
+    private final Sql2o sql2o;
+
+    public Sql2oUserDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(User user) {
+        String sql = "INSERT INTO users (userName, userCompanyPosition, userCompanyRole, departmentId) VALUES (:userName, :userCompanyPosition, :userCompanyRole, :departmentId)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM users")
+                    .executeAndFetch(User.class);
+        }
+    }
+
+    @Override
+    public User findUserById(int id) {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM users WHERE id=:id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(User.class);
+        }
+    }
+
+    @Override
+    public void update(int id, String newUserName, String newUserCompanyPosition, String newUserCompanyRole, int departmentId) {
+        String sql = "UPDATE users SET (userName, userCompanyPosition, userCompanyRole, departmentId) = (:userName, :userCompanyPosition, :userCompanyRole, :departmentId) WHERE id=:id"; //CHECK!!!
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("userName", newUserName)
+                    .addParameter("userCompanyPosition", newUserCompanyPosition)
+                    .addParameter("userCompanyRole", newUserCompanyRole)
+                    .addParameter("departmentId", departmentId)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from users WHERE id = :id"; //raw sql
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public void clearAll() {
+        String sql = "DELETE FROM users";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql).executeUpdate();
+        }catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+}
---
+++ src/test/java/dao/Sql2oUserDaoTest.java
@@ -0,0 +1,10 @@
+package dao;
+import models.User;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.sql2o.Sql2o;
+import org.sql2o.Connection;
+
+public class Sql2oUserDaoTest {
+}
